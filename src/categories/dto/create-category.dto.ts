220b518388fbb87db9413398ff42b33d5607685f
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-10 01:09:28 +0500

    added Class Validator

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,3 +1,12 @@
+import {
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator/types/decorator/decorators';
+
 export class CreateCategoryDto {
+  @MinLength(2)
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
+import { MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator/types/decorator/decorators';
+
 export class CreateUserDto {
+  @MinLength(2)
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
   password: string;
   //   posts: string[];
 }
