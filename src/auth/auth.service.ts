8d0ff40e3046ecafca534f7bb6b2e1aec19e7514
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-10 03:58:58 +0500

    commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateUser(email: string, password: string) {}
+  registerUser(createUserDto: CreateUserDto) {}
+}
